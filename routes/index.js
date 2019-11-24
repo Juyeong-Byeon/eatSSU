480b2edb7039948780271c1bb2f0346a3d673ef6
wndudqus <35381940+wndudqus@users.noreply.github.com>
2019-11-24 19:46:51 +0900

    git open
    지금까지 작업 한 것 업로드 합니다
    1. 각종 패키지 인클루드
    2. SQL 연결
    3. 탭기능 구현

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const mysql=require('mysql');
+router.use(express.static('public'));
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '654654',
+  database : 'eatSSU',
+  port :3307
+});
+
+connection.connect();
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.query.category);
+  connection.query(
+    "SELECT *FROM restaurant WHERE category='"+req.query.category+"'",(error,results,fields)=>{
+      if(error) console.log(error);
+      else{
+      console.log(results);
+      res.render('indexeatSSU', { title: 'eatSSU' ,tableShow:results});
+      }
+    }
+    );
+ 
+});
+
+router.get('/storeName:',(req,res,next)=>{
+
+  res.send('OK');
+
+
+});
+
+module.exports = router;
