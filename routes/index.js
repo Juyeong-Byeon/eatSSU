9e4d77459e0617944de72a4179ade3906d154824
wndudqus <35381940+wndudqus@users.noreply.github.com>
2019-11-26 14:45:21 +0900

    search 기능구현, 랜덤 기능 view 구현
    1. search 기능구현
    2. 랜덤 기능 view 구현

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ const mysql=require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'dbgmlehd',
+  password : '654654',
   database : 'eatSSU',
-  port :3306
+  port :3307
 });
 
 connection.connect();
@@ -28,7 +28,7 @@ router.get('/', function(req, res, next) {
       res.render('indexeatSSU', { title: 'eatSSU' ,tableShow:results});
       }
     }
-  );
+    );
   }else{
     connection.query(
     "SELECT *FROM restaurant",(error,results,fields)=>{
@@ -39,7 +39,9 @@ router.get('/', function(req, res, next) {
         }
       }
     );
+
   }
+ 
 });
 
 /* GET home page. */
@@ -47,8 +49,8 @@ router.get('/kind/:name', function(req, res, next) {
  
   let category=req.params.name
   console.log(req.params.name);
-  if(category!='all'){
-    connection.query(
+  if(category!='all'&&category!='search'){//현재 단계에서 특수한 경우는 category가 all 이거나  search 인 경우 밖에 없음  두 경우면 fll off
+    connection.query(//탭에 따른 쿼리
       "SELECT *FROM restaurant WHERE category='"+req.params.name+"'",(error,results,fields)=>{
         if(error) console.log(error);
         else{
@@ -56,26 +58,41 @@ router.get('/kind/:name', function(req, res, next) {
         res.render('indexeatSSU', { title: 'eatSSU' ,tableShow:results, name: category});
         }
       }
-    );
-  }else{
+      );  
+  }else if(category=='search'){//search를 할 경우 쿼리문을 읽어와 like 문으로 처리해줌
+    console.log("!!!!!"+req.query.searchKeyword);
     connection.query(
-      "SELECT *FROM restaurant",(error,results,fields)=>{
+      "SELECT *FROM restaurant WHERE storeName LIKE '%"+req.query.searchKeyword+"%'",(error,results,fields)=>{
         if(error) console.log(error);
         else{
         console.log(results);
-        res.render('indexeatSSU', { title: 'eatSSU' ,tableShow:results });
+        res.render('indexeatSSU', { title: 'eatSSU' ,tableShow:results, name: category});
         }
       }
-    );
+      );  
   }
+  else{
+      connection.query(//all path로 갈경우 모든 데이터를
+        "SELECT *FROM restaurant",(error,results,fields)=>{
+          if(error) console.log(error);
+          else{
+          console.log(results);
+          res.render('indexeatSSU', { title: 'eatSSU' ,tableShow:results });
+          }
+        }
+        );
+  }
+ 
 });
 
 router.get('/storeName:',(req,res,next)=>{
   res.send('OK');
 });
 
-// get random page
+
 router.get('/kind/:name/random', function(req, res, next) {
+  
+  
   let category=req.params.name
   console.log(category);
   if(category!="all"){
@@ -86,7 +103,8 @@ router.get('/kind/:name/random', function(req, res, next) {
         console.log(results);
         const randomIndex = Math.floor(Math.random() *  results.length);
         console.log(randomIndex);
-        res.render('random', { title: 'eatSSU' ,store:results[randomIndex]});
+        res.render('random', { title:'eatSSU' ,results: results[randomIndex]});
+        console.log(results[randomIndex]);
         }
       }
       );
@@ -99,28 +117,14 @@ router.get('/kind/:name/random', function(req, res, next) {
           console.log(results);
           const randomIndex = Math.floor(Math.random() *  results.length);
           console.log(randomIndex);
-          res.render('random', { title: 'eatSSU' ,store:results[randomIndex]});
+          
+          res.render('random', { title: 'eatSSU' ,results:results[randomIndex]});
+          
           }
         }
         );
   }
-});
 
-// get store page
-router.get('/kind/:name/:storeName', function(req, res, next) {
-  let category=req.params.name
-  let storeName=req.params.storeName
-  console.log(req.params);
-  connection.query(
-    `SELECT *FROM restaurant WHERE category='${category}' and storeName='${storeName}'`,(error,results,fields)=>{
-      if(error) console.log(error);
-      else{
-      console.log('hi');
-      console.log(results);
-      res.render('review', { title: 'eatSSU' ,storeName:results.storeName});
-      }
-    }
-  );
 });
 
 module.exports = router;
