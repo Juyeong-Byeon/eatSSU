b5298266921079e96c6b4c5b73c1eaa4fd934248
wndudqus <35381940+wndudqus@users.noreply.github.com>
2019-11-28 18:24:26 +0900

    리뷰페이지 정보 전달(음식점 정보, 리뷰정보)/pug 사진 불러올 수 있도록 수정
    음식점 정보와 리뷰 정보 전달할 수 있도록 수정했습니다./pug는 사진을 불러올 수 있도록 수정하였습니다.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ var connection = mysql.createConnection({
   user     : 'root',
   password : '654654',
   database : 'eatSSU',
-  port :3307
+  port :3307,
+  multipleStatements: true
 });
 
 connection.connect();
@@ -61,7 +62,6 @@ router.get('/kind/:name', (req, res, next)=>{
       }
       );  
   }else if(category=='search'){//search를 할 경우 쿼리문을 읽어와 like 문으로 처리해줌
-    console.log("!!!!!"+req.query.searchKeyword);
     connection.query(
       "SELECT *FROM restaurant WHERE storeName LIKE '%"+req.query.searchKeyword+"%'",(error,results,fields)=>{
         if(error) console.log(error);
@@ -97,12 +97,12 @@ router.get('/storeName:',(req,res,next)=>{
 //////////////////////////////////////////////////////////////////////////////////////랜덤뽑기,각 리뷰페이지 구현파트
 
 router.get('/kind/:category/:storeName', (req, res, next)=>{
+  let category=req.params.category
   console.log(req.params.storeName);
-  if(req.params.storeName=='random'){
-    let category=req.params.category
+  if(req.params.storeName=='random'){//렌덤일 시에 쿼리를 진행한다. 
     console.log(category);
     if(category!="all"){
-      connection.query(
+      connection.query(//각 탭에 맞는 값만 불러와서 랜덤뽑기를 진행 할 때 
         "SELECT *FROM restaurant WHERE category='"+category+"'",(error,results,fields)=>{
           if(error) console.log(error);
           else{
@@ -116,7 +116,7 @@ router.get('/kind/:category/:storeName', (req, res, next)=>{
       );
     }
     else{
-        connection.query(
+        connection.query(//모든 값을 불러와서 랜덤을 진행해야 할 때,
           "SELECT *FROM restaurant",(error,results,fields)=>{
             if(error) console.log(error);
             else{
@@ -131,23 +131,57 @@ router.get('/kind/:category/:storeName', (req, res, next)=>{
           );
     }
   }
-  else{
+  else{//랜덤쿼리가 아닐경우.
     connection.query(//탭에 따른 쿼리
-      "SELECT *FROM restaurant WHERE storeName='"+req.params.storeName+"'",(error,results,fields)=>{
+      `SELECT *FROM restaurant WHERE storeName='${req.params.storeName}';SELECT *FROM review WHERE storeName='${req.params.storeName}'`,(error,results,fields)=>{
         if(error) console.log(error);
         else{
-        console.log(results);
-        res.render('review',{storeInfo:results});
+        console.log(results[0],results[1]);
+        
+        res.render('review',{storeInfo:results[0],reviews:results[1]});
         }
       }
       );
   }
-
-
-  
-
 });
 
+
+router.post('/kind/:category/:storeName',(req,res,next)=>{//리뷰를 작성했을 때 post로 받음. 
+  let sqls=[]; //C/R/U/D 기능을 위한 쿼리문들.
+  sqls[0]=`INSERT INTO review VALUE('${reviewID}','${req.params.storeName}','${req.body.nickname}','${req.body.password}','${req.body.reviewDesc}',now());`;//C기능
+  sqls[1]=`UPDATE review SET reviewDesc='${req.body.reviewDesc}' where reviewID=(SELECT reviewID FROM review WHERE nickname='${req.body.nickname}' AND password='${req.body.password}')`;//U
+  sqls[2]=`DELETE From review where reviewID=(SELECT reviewID FROM review WHERE nickname='${req.body.nickname}' AND password='${req.body.password}');`;//D
+  //위 코드상에서 바로 닉네임과 패스워드를 선택해서 삭제해도 되지만 review 릴레이션의 key는 reviewID 와 storeName 이기 때문에 이렇게 쿼리를 작성하였다.
+  sqls[3]=`SELECT *FROM restaurant WHERE storeName='${req.params.storeName}';`;//R기능
+  sqls[4]=`SELECT *FROM review WHERE storeName='${req.params.storeName};`;//R기능
+
+  /*
+  DELETE From review where reviewID='리뷰아이디';
+UPDATE review SET discription='수정 내용' where reviewID=(select reviewID FROM review where nickname=''AND password='');
+  들어가야 하는 정보들
+  req.params.storeName
+
+  req.body.nickname
+  req.body.password
+  req.body.reviewDesc
+  req.body.date<=이친구는 그냥 now()로 대체 
+  */
+
+
+
+
+ connection.query(//탭에 따른 쿼리
+  `${sqls[0]+sqls[1]+sqls[2]}`,(error,results,fields)=>{
+
+    console.log(results[0]);
+    if(error) console.log(error);
+    else{
+    console.log(results);
+    res.render('review',{storeInfo:results[0],reviews:results[1]});
+    }
+  });
+  }
+);
 
 
 
