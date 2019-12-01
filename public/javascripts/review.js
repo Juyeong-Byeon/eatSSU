763a4b63fd858129906437279bc3083f54658fb0
wndudqus <35381940+wndudqus@users.noreply.github.com>
2019-12-02 01:54:51 +0900

    js파일 연결이 잘 못 되어있어서 수정하였습니다.
    js파일 연결이 잘 못 되어있어서 수정하였습니다.

--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -15,3 +15,30 @@ function checkNull(){
 
 
 }
+
+
+function deleteReview(reviewID,nickname,password){
+    console.log("review"+reviewID);
+
+    let inputpassword=prompt('비밀번호:');
+    if (inputpassword==password) {
+        return true;
+        
+    }else{
+        alert('비밀번호가 틀렸습니다.!');
+        return false;
+    }
+
+
+}
+
+
+
+
+window.addEventListener('load',()=>{
+
+    console.log('hi');
+
+
+
+});
